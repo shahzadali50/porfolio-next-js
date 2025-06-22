18a597c0a9af139f20ce7200dbffcdf6c07f2846
Shahzad Ali <108119561+shahzadali50@users.noreply.github.com>
2025-06-22 17:11:54 +0500

    remove experience\

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Banner } from "./pages/component/Banner";
 import { About } from "./pages/component/About"; 
 import { Skills } from "./pages/component/Skills"; 
 // import { Experience } from "./pages/component/Experience"; 
-import { Projects } from "./pages/component/Projects"; 
+// import { Projects } from "./pages/component/Projects"; 
 import { Faq } from "./pages/component/Faq"; 
 import { Contact } from "./pages/component/Contact"; 
 import { Footer } from "./pages/component/Footer"; 
@@ -17,7 +17,7 @@ export default function Home() {
     <About/>
     <Skills/>
     {/* <Experience/> */}
-    <Projects/>
+    {/* <Projects/> */}
     <Faq/>
     <Contact/>
     <Footer/>
